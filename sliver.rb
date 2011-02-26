5b5bd0f09d76688ffd16bb79ccc60f1c6a4d9e73
Eric Budd <ebudd@zavenetworks.com>
2011-02-25 18:04:58 -0600

    Adding initial functionality to convert an array to HTML

---
+++ sliver.rb
@@ -0,0 +1,44 @@
+class Array
+  # attr_reader :htmled
+
+  def to_html
+    if first.is_a?(Array) 
+      self.each do |x|
+        unless x.is_a?(Array)
+          raise ArgumentError.new(":#{x.to_s} is not a valid tag array.  Did you mean [:#{x.to_s}]?")
+        end
+      end
+      map{|x| x.to_html }.join # traverse
+    else
+      self.to_tag #descend
+    end
+  end
+
+  def to_tag
+    return '' if empty?
+    return @htmled = "<#{self[0].to_s} />" if self.size == 1
+
+    list = reject{ |x| x.is_a?(Hash) }
+    head, tail = list.bite_off_head
+
+    fore_tag = [head.to_s, properties.to_html].compact.reject{ |x| x.empty? }.join(' ')
+
+    return "<#{fore_tag} />" if tail.nil? || tail.empty?
+    "<#{fore_tag}>#{tail.to_html}</#{head}>"
+  end
+
+  def properties
+    self.inject({}) { |acc, elem| acc.merge!(elem) if elem.is_a?(Hash); acc }
+  end
+
+  def bite_off_head
+    return self[0], self[1..-1]
+  end
+end
+
+class Hash
+  def to_html
+    self.map{ |k, v| "#{k.to_s}=\"#{v.to_s}\""}.join(' ')
+  end
+end
+
---
+++ sliver_spec.rb
@@ -0,0 +1,75 @@
+# require 'rubygems'
+require 'spec/autorun'
+require 'sliver'
+
+describe "Sliver" do
+  context "while entagging data" do
+    it "entags a tag array" do
+      data = [[:a]]
+      data.to_html.should == '<a />'
+    end
+
+    it "entags multiple arrays" do
+      data = [[[:a], [:b]]]
+      data.to_html.should == '<a /><b />'
+    end
+
+    it "entags nested tag arrays" do
+      data = [[:a, [:b]]]
+      data.to_html.should == '<a><b /></a>'
+    end
+
+    it "entags nested multiple tag arrays" do
+      data = [[[:a, [:b]], [:c, [:d]]]]
+      data.to_html.should == '<a><b /></a><c><d /></c>'
+    end
+
+    it "entags nested arrays to an arbitrary depth" do
+      data = [[:a, [:b, [:c, [:d]]]]]
+      data.to_html.should == '<a><b><c><d /></c></b></a>'
+    end
+
+    it "ignores empty nesting" do
+      data = [[:a, [[[:b]]], [:c]]]
+      data.to_html.should == '<a><b /><c /></a>'
+    end
+
+    it "raises an error if a tag is not correctly wrapped" do
+      data = [[:a], :c]
+      lambda { data.to_html }.should raise_error(":c is not a valid tag array.  Did you mean [:c]?")
+    end
+
+    context "with properties" do
+      it "entags a tag array" do
+        data = [[:a, {:foo => :bar}]]
+        data.to_html.should == '<a foo="bar" />'
+      end
+
+      it "entags properties around interleaved tag arrays" do
+        data = [[:a, [:b], {:foo => :bar}]]
+        data.to_html.should == '<a foo="bar"><b /></a>'
+      end
+
+      it "collects multiple hashes in to a single list of properties" do
+        data = [[:a, {:foo => :bar}, {:baz => 'quux!'}]]
+        data.to_html.should match(/baz="quux!"/)
+        data.to_html.should match(/foo="bar"/)
+      end
+
+      it "overrides properties which have already been defined to the left" do
+        data = [[:a, {:foo => :bar}, {:foo => 'quux!'}]]
+        data.to_html.should == '<a foo="quux!" />'
+      end
+
+      it "entags nested arrays" do
+        data = [[:a, [:b, {:lol => 'wut'}]]]
+        data.to_html.should == '<a><b lol="wut" /></a>'
+      end
+
+      it "entags nested multiple tag arrays" do
+        data = [[[:a, [:b, {:lol => 'wut'}]], [:c, [:d, {:no => 'wai'}]]]]
+        data.to_html.should == '<a><b lol="wut" /></a><c><d no="wai" /></c>'
+      end
+    end
+  end
+end
