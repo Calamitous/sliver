b1f1fa160cbd90a59d7835e5a3fa6313b7324c17
Eric Budd <ebudd@zavenetworks.com>
2011-02-27 00:48:04 -0600

    Began working out the templater

--- lib/sliver.rb
+++ lib/sliver.rb
@@ -8,5 +8,35 @@ class Sliver
   def initialize(doc)
     @doc = Nokogiri::HTML(doc)
   end
+
+  def self.load_template(filename)
+    self.new(File.read(filename))
+  end
+
+  def set_content(selector, data)
+    puts data.inspect
+    selected = @doc.at_css(selector)
+    
+    raise "No element found in template for selector \"#{selector}\"" unless selected
+    selected.add_child (data.is_a?(Array) ? data.to_html : data.to_s)
+  end
+
+  def render
+    @doc.to_html
+  end
+
+  def render_to_file(filename = 'test2.html')
+    File.open(filename, 'w') { |f| f.write(self.render) }
+  end
+
+  def self.test
+    template = Sliver.load_template('test.html')
+    things = ['Thing 1', 'Thing 2', 'NO WAI WAT']
+
+    template.set_content('.test', "Go to Home")
+    template.set_content('#list', things.map{|x| [:p, x]})
+    template.render_to_file
+    template
+  end
 end
 
