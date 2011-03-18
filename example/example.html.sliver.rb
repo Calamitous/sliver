b9642e70b3a9c370aec5c74bd8225ef922ec64be
Eric Budd <ebudd@zavenetworks.com>
2011-03-18 14:41:49 -0500

    Fixed example template bugs

--- example/example.html.sliver.rb
+++ example/example.html.sliver.rb
@@ -2,11 +2,14 @@ require File.expand_path('../../lib/sliver/template', __FILE__)
 
 # load './example.html.sliver.rb'; ExampleView.new.render_to_file('test2.html')
 
+# load '../lib/sliver.rb'; load './example.html.sliver.rb'; ev = ExampleView.new; a = ev.make_repeating_sub('#list')
+#
 class ExampleView < Sliver::Transformer
 
   def initialize(data = {})
     @data = data
-    super File.basename(__FILE__, '.sliver.rb')
+    #super File.basename(__FILE__, '.sliver.rb')
+    super './example.html'
   end
 
   # use_template 'test.html'
@@ -14,10 +17,11 @@ class ExampleView < Sliver::Transformer
     change('h1', 'Zorbo\'s Awesome Website')
     add_into('.link', name)
     change('#list', list_snippet('George'))
+    add_into('.link', 'name')
 
     set_attributes('.link', {:href => home_url})
-    set_class('.selected', 'chosen')
-    delete('#admin_login') unless @data[:is_admin]
+    #set_class('.selected', 'chosen')
+    #delete('#admin_login') unless @data[:is_admin]
     super
   end
 
--- lib/sliver/transformer.rb
+++ lib/sliver/transformer.rb
@@ -1,6 +1,6 @@
 require File.expand_path('../../sliver', __FILE__)
 
-class Sliver::Transformer < Sliver
+class Sliver::Transformer < Sliver::Template
   def initialize(filename)
     # super.new(File.read(filename))
     super File.read(filename)
